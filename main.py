2b03a21462539002eaaec0257a29ba1dea937f26
ZsomborHodi <96060351+hodizsombor@users.noreply.github.com>
2021-12-13 12:53:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+szam = int(input('adj meg egy számot. '))
+szam2 = int(input('adj meg még egy számot. '))
+összeg = szam + szam2
+külö = szam - szam2
+szorz = szam * szam2
+print (összeg)
+print (külö)
+print (szorz)
